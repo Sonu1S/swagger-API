45b69f2675d8e89800e7b6a0b934794990de81f3
sonu <sonukrsharma1299@gmail.com>
2023-10-28 17:51:57 +0530

    now only springBoot restFull webservices

--- springBoot-Blog--RestAPI/src/main/java/net/javaguide/springboot/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguide.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguide.springboot.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
